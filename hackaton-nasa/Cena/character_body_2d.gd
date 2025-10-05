d1b0676dd2fc6ae3defdbf9166df0c28acd5398e
RenanRabelo1 <renanrabelo82@gmail.com>
2025-10-04 22:37:47 -0300

    Falta coisa

--- hackaton-nasa/Cena/character_body_2d.gd
+++ hackaton-nasa/Cena/character_body_2d.gd
@@ -6,14 +6,9 @@ func _physics_process(delta: float) -> void:
 	# Get input direction for both axes
 	var direction_x := Input.get_axis("esquerda", "direita")
 	var direction_y := Input.get_axis("cima", "baixo")
-	
-	# Calculate velocity for both axes
 	velocity.x = direction_x * SPEED
 	velocity.y = direction_y * SPEED
-	
-	# Animations
 	update_animations(direction_x, direction_y)
-	
 	move_and_slide()
 
 func update_animations(direction_x: float, direction_y: float) -> void:
@@ -33,3 +28,9 @@ func update_animations(direction_x: float, direction_y: float) -> void:
 		$AnimatedSprite2D.flip_h = false
 	else:
 		$AnimatedSprite2D.play("idle")  # Animação quando parado
+
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	$AnimatedSprite2D2.show()
+	$Label1.show()
--- hackaton-nasa/Cena/rigid_body_2d.gd
+++ hackaton-nasa/Cena/rigid_body_2d.gd
@@ -1,6 +1,5 @@
-extends Area2D
-signal entrou
-var entrada=0
+extends RigidBody2D
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,10 +9,3 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-func _on_body_entered(body: Node2D) -> void:
-	entrada+=1
-	if (entrada==1):
-		$AnimatedSprite2D.play("trigo_crescendo")
-		emit_signal("entrou")
-	
